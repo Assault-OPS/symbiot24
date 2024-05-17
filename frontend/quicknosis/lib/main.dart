dc5734d437039d74665e7538194dfa8bcedcfc98
Assault-OPS <anu.2004.mys@gmail.com>
2024-05-17 17:10:17 +0530

    finished menu and form pages

---
+++ frontend/quicknosis/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../pages/form.dart';
+import '../pages/symptoms.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quicknosis',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Quicknosis Menu'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Quicknosis Menu",
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.lightBlue,
+        ),
+      backgroundColor: Colors.white,
+      
+      body: Center(
+        child: IconButton(
+          icon: Icon(Icons.medical_services_rounded),
+          iconSize: 200.0,
+          color: Colors.blue,
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+          },
+        ),
+      )
+    );
+  }
+}
---
+++ frontend/quicknosis/lib/pages/form.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import '../pages/symptoms.dart';
+
+class Home extends StatefulWidget{
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final formKey = GlobalKey<FormState>(); //key for form
+  String? username = "";
+  int? age = 0;
+  String? gender = "";
+  bool isSubmitted = false;
+  @override
+  Widget build(BuildContext context) {
+    final double height= MediaQuery.of(context).size.height;
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return  Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: Color(0xFFffffff),
+        body: Container(
+          padding: const EdgeInsets.only(left: 40, right: 40),
+          child: Form(
+            key: formKey, //key for form
+            child:Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height:height*0.04),
+                Text("Patient Details", style: TextStyle(fontSize: 30, color:Color(0xFF363f93)),),
+                SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter name"
+                  ),
+                  validator: (value) {
+                    if(value!.length < 4) return "Please enter more than 4 characters";
+                    else return null;
+                  },
+                  onSaved: (value) => setState(() => username = value)
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter age"
+                  ),
+                  validator: (value) {
+                    if(RegExp(r'[a-zA-Z]').hasMatch(value!)) return "Please enter a number";
+                    else return null;
+                  },
+                  onSaved: (value) => setState(() => age = int.tryParse(value!))
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter gender"
+                  ),
+                  onSaved: (value) => setState(() => gender = value)
+                ),
+                SizedBox(height: 50),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      child: Text("Submit"),
+                      onPressed: () {
+                        final isValid = formKey.currentState!.validate();
+                        if(isValid) {
+                          formKey.currentState!.save();
+                          isSubmitted = true;
+                          print(username);
+                          print(age);
+                          print(gender);
+                        }
+                      }
+                    ),
+                    SizedBox(width: 25),
+                    ElevatedButton(
+                      child: Text("Continue"),
+                      onPressed: () {
+                        if(isSubmitted)
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => Symptoms()));
+                        else {
+                          final String message = "Please submit the form before you proceed";
+                          final snackBar = SnackBar(
+                            content: Text(message, style: TextStyle(fontSize: 20)),
+                            backgroundColor: Colors.red
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }                          
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white
+                      )
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
